9660315c96eef16d2d085cad5cd225683d875e1a
Itamar Schwartz <itamarschwartz10@gmail.com>
2023-06-08 18:59:49 +0300

    Move things to src directory

--- lib/orbit_standard_library.dart
+++ lib/orbit_standard_library.dart
@@ -1,10 +1,12 @@
 library orbit_standard_library;
 
-export "package:orbit_standard_library/optional.dart" show Optional, Some, None;
-export "package:orbit_standard_library/map_nullable.dart" show MapNullable;
-export "package:orbit_standard_library/map_optional.dart" show MapOptional;
-export "package:orbit_standard_library/average_or_null.dart" show AverageOrNull;
-export "package:orbit_standard_library/carousel_with_indicator.dart"
+export "package:orbit_standard_library/src/optional.dart"
+    show Optional, Some, None;
+export "package:orbit_standard_library/src/map_nullable.dart" show MapNullable;
+export "package:orbit_standard_library/src/map_optional.dart" show MapOptional;
+export "package:orbit_standard_library/src/average_or_null.dart"
+    show AverageOrNull;
+export "package:orbit_standard_library/src/carousel_with_indicator.dart"
     show CarouselWithIndicator;
-export "package:orbit_standard_library/tuple_extensions.dart"
+export "package:orbit_standard_library/src/tuple_extensions.dart"
     show TupleExtensions;
