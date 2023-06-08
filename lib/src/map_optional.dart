4a5e89779bf2a9fb8b648ff1390ab903701dbbb7
Itamar Schwartz <itamarschwartz10@gmail.com>
2023-06-08 19:37:11 +0300

    Linting errors

--- lib/src/map_optional.dart
+++ lib/src/map_optional.dart
@@ -1,22 +1,22 @@
-import 'package:orbit_standard_library/src/optional.dart';
+import "package:orbit_standard_library/src/optional.dart";
 
-import 'map_nullable.dart';
+import "package:orbit_standard_library/src/map_nullable.dart";
 
 extension MapOptional<A> on Optional<A> {
   B fold<B>(final B Function() onEmpty, final B Function(A) onSome) =>
       switch (this) {
-        Some(some: var some) => onSome(some),
-        None() => onEmpty()
+        Some<A>(some: final A some) => onSome(some),
+        None<A>() => onEmpty()
       };
 
   Optional<B> mapNullable<B>(final B Function(A) f) => switch (this) {
-        Some(some: var some) => Some(f(some)),
-        None() => None(),
+        Some<A>(some: final A some) => Some<B>(f(some)),
+        None<A>() => None<B>(),
       };
 
   Optional<B> onNullDo<B>(final B Function() f) => switch (this) {
-        Some(some: var _) => None(),
-        None() => Some(f()),
+        Some<A>(some: _) => None<B>(),
+        None<A>() => Some<B>(f()),
       };
 
   Optional<B> onNull<B>(final B value) => onNullDo(always(value));
