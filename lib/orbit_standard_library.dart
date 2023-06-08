7bc89a9c999259e9f6b8347869669c0db684d00b
Itamar Schwartz <itamarschwartz10@gmail.com>
2023-06-08 19:37:54 +0300

    Add widgets to library

--- lib/orbit_standard_library.dart
+++ lib/orbit_standard_library.dart
@@ -6,7 +6,12 @@ export "package:orbit_standard_library/src/map_nullable.dart" show MapNullable;
 export "package:orbit_standard_library/src/map_optional.dart" show MapOptional;
 export "package:orbit_standard_library/src/average_or_null.dart"
     show AverageOrNull;
-export "package:orbit_standard_library/src/carousel_with_indicator.dart"
+
+export "package:orbit_standard_library/src/widgets/carousel_with_indicator.dart"
     show CarouselWithIndicator;
+
+export "package:orbit_standard_library/src/widgets/selector.dart" show Selector;
+export "package:orbit_standard_library/src/widgets/switcher.dart" show Switcher;
+
 export "package:orbit_standard_library/src/tuple_extensions.dart"
     show TupleExtensions;
